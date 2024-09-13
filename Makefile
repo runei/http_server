cd4190cfebcbcc2e64b62c1813bdcf8c0219cc6c
Leonardo Invernizzi <lminvernizzi@gmail.com>
2024-09-13 02:41:05 +0000

    Added code coverage

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
-CMAKE_BUILD_DIR = build
 CMAKE_SOURCE_DIR = $(shell pwd)
+CMAKE_BUILD_DIR = $(CMAKE_SOURCE_DIR)/build
+CMAKE_COVERAGE_DIR = $(CMAKE_BUILD_DIR)/coverage
 
-.PHONY: build clean run run-tests format cppcheck
+SRC_DIR = $(CMAKE_SOURCE_DIR)/src
+INCLUDE_DIR = $(CMAKE_SOURCE_DIR)/include
+TESTS_DIR = $(CMAKE_SOURCE_DIR)/tests
+
+.PHONY: build clean run run-tests format cppcheck coverage
 
 build:
 	@echo "Creating build folder"
@@ -19,13 +24,27 @@ clean:
 run:
 	@./$(CMAKE_BUILD_DIR)/src/Main
 
-run-tests:
+test:
 	@cd $(CMAKE_BUILD_DIR) && ctest --output-on-failure
 
 format:
 	@echo "Formating files"
-	@find src tests -name "CMakeLists.txt" -exec cmake-format -i {} \;
+	@find $(SRC_DIR) $(TESTS_DIR) -name "CMakeLists.txt" -exec cmake-format -i {} \;
 
 cppcheck:
 	@cppcheck --enable=all -Iinclude --error-exitcode=1 --suppress=missingIncludeSystem --inconclusive src/
 # @cppcheck --enable=all -Iinclude -I/usr/include -I/usr/include/CppUTest --error-exitcode=1 --suppress=missingIncludeSystem --suppress=*:*/usr/include/CppUTest/* --inconclusive tests/
+
+coverage: clean
+	@echo "Building project with coverage flags"
+	@mkdir -p $(CMAKE_BUILD_DIR)
+	@mkdir -p $(CMAKE_COVERAGE_DIR)
+	@cd $(CMAKE_BUILD_DIR) && cmake -DCMAKE_BUILD_TYPE=Coverage $(CMAKE_SOURCE_DIR)
+	@cd $(CMAKE_BUILD_DIR) && make
+	@echo "Running tests for coverage"
+	@cd $(CMAKE_BUILD_DIR) && ctest --output-on-failure
+	@echo "Generating coverage report using gcov"
+	@find $(CMAKE_BUILD_DIR) -name "*.gcno" -exec gcov {} \;
+	@echo "Moving coverage reports to $(CMAKE_COVERAGE_DIR)"
+	@find . -name "*.gcov" -exec mv {} $(CMAKE_COVERAGE_DIR) \;
+	@echo "Coverage report generated in $(CMAKE_COVERAGE_DIR)"
--- project.sh
+++ project.sh
@@ -1,43 +1,52 @@
 #!/bin/bash
 
+# Create a _bash_history file if it doesn't exist
+touch ".devcontainer/_bash_history"
+
 CONTAINER_NAME="gcc-container"
 
-isContainerRunning() {
+is_container_running() {
     docker ps | grep "$CONTAINER_NAME" >/dev/null
 }
 
-getContainerId() {
-    docker ps -a -q --filter "name=$CONTAINER_NAME"
+stop_container() {
+    if is_container_running; then
+        echo "Stopping the container..."
+        docker stop "$CONTAINER_NAME"
+    else
+        echo "Container is not running."
+    fi
 }
 
-enterContainer() {
-    CONTAINER_ID=$(getContainerId)
+start_container() {
+    docker-compose up -d --build
+}
 
-    if [ -n "$CONTAINER_ID" ]; then
-        if isContainerRunning; then
-            echo "Connecting to environment..."
-            echo "Container ID: $CONTAINER_ID"
-
-            if command -v winpty >/dev/null 2>&1; then
-                winpty docker exec -it "$CONTAINER_ID" bash
-            else
-                docker exec -it "$CONTAINER_ID" bash
-            fi
+run_container() {
+    if is_container_running; then
+        echo "Environment already running - connecting..."
+        if command -v winpty >/dev/null 2>&1; then
+            winpty docker exec -it "$CONTAINER_NAME" bash
         else
-            echo "Container is not running. Please start the container first."
+            docker exec -it "$CONTAINER_NAME" bash
         fi
     else
-        echo "No container found with a name containing \"$CONTAINER_NAME\""
+        echo "Container is not running. Please start the container first."
     fi
 }
 
 # Check the first argument
 case "$1" in
-    devenv)
-        enterContainer
+    start)
+        start_container
+        ;;
+    run)
+        run_container
+        ;;
+    stop)
+        stop_container
         ;;
     *)
-        echo "Usage $0 {devenv}"
+        echo "Usage: $0 {start|run|stop}"
         exit 1
-        ;;
 esac
