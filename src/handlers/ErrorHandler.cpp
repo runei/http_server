f7057c1c187395e79f55c946973d2fb261f045eb
Leonardo Invernizzi <lminvernizzi@gmail.com>
2024-10-03 19:33:21 +0000

    Error Handler

---
+++ src/handlers/ErrorHandler.cpp
@@ -0,0 +1,27 @@
+#include "ErrorHandler.hpp"
+
+#include <sstream>
+
+HttpResponse ErrorHandler::handleError(HttpStatusCode status_code)
+{
+    HttpResponse::Builder builder;
+
+    builder.setHttpVersion(HttpVersion::Http11).setStatusCode(status_code).setBody(generateErrorMessage(status_code));
+
+    builder.addHeader("Content-Type", "text/html");
+
+    return builder.build();
+}
+
+std::string ErrorHandler::generateErrorMessage(HttpStatusCode status_code)
+{
+    auto status_description = HttpStatusCodeHelper::getHttpStatusDescription(status_code);
+
+    std::ostringstream error_page;
+    error_page << "<html><head><title>" << status_description << "</title></head>"
+               << "<body><h1>" << status_description << "</h1>"
+               << "<p>An error occurred: " << status_description << "</p>"
+               << "</body></html>";
+
+    return error_page.str();
+}
---
+++ src/handlers/ErrorHandler.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "HttpResponse.hpp"
+#include "HttpStatusCode.hpp"
+
+class ErrorHandler
+{
+public:
+    static HttpResponse handleError(HttpStatusCode status_code);
+
+private:
+    static std::string generateErrorMessage(HttpStatusCode status_code);
+};
